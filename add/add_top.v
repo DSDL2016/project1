9028e57455dbf244966789dd9f9728e3b71de6a4
Liu Yen-Ting <windows.linux.mac@gmail.com>
2016-04-09 21:22:37 +0800

    Add top module of ADD

---
+++ add/add_top.v
@@ -0,0 +1,31 @@
+module add_top(
+	parameter width = 6
+)(
+	input		[width-1:0]	a,
+	input		[widht-1:0]	b,
+	output	[width-1:0]	sum,
+	output					overflow
+);
+	
+	// additional bit of carry array is for overflow signal
+	wire carry[width:0];
+	
+	// TODO: use carry look ahead instead of ripple
+	genvar i;
+	generate
+		for(i = 0; i < width; i = i+1) begin
+			full_adder fa(
+				.a			(a[i]),
+				.b			(b[i]),
+				.c_in		(carry[i]),
+				.sum		(sum[i]),
+				.c_out	(carry[i+1])
+			);
+		end
+	endgenerate
+	
+	// first bit is never 1'b1
+	assign carry[0] = 1'b0;
+	assign overflow = carry[width];
+	
+endmodule
