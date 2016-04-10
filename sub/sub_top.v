d3fbf9406bf05a90a069700a0a90782d3711854b
Liu Yen-Ting <windows.linux.mac@gmail.com>
2016-04-10 09:59:43 +0800

    Add negate module for SUB

--- sub/sub_top.v
+++ sub/sub_top.v
@@ -6,5 +6,21 @@ module sub_top #(
 	output [width-1:0] out,
 	output				 overflow
 );
+	
+	// SUB = ADD and NEG
+	
+	wire [width-1:0] neg_b;
+	
+	neg_top neg(
+		.in  (b),
+		.out (neg_b)
+	);
+	add_top add(
+		.c_in		 (1'b1),
+		.a			 (a),
+		.b			 (neg_b),
+		.out		 (out),
+		.overflow (overflow)
+	);
 
 endmodule
