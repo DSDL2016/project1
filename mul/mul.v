7c3f39e9d4b5b76474e2f0c1c99c9893d9f62897
tinray <b03902072@ntu.edu.tw>
2016-04-10 18:21:21 +0800

    take sign into consideration

--- mul/mul.v
+++ mul/mul.v
@@ -9,35 +9,55 @@ module mul #(
    integer                 sum = 0;
    integer                 i = 0;
    integer                 sign = 1;
+   integer                 i_a;
    integer                 i_b;
-   
+   integer                 mask = 31;
+                
    
    always @( a, b )   
      begin         
         sum = 0;
+        i_a = a[width - 2: 0];           
+        i_b = b[width - 2: 0];           
+        sign = 1;        
         
-        // deal with sign
+        // $display( "i_a = %b, i_b = %b\n", i_a, i_b);
+        
+        // extract the number part
+        if( a[width - 1] )begin
+           sign = -sign;
+           i_a = (~i_a + 1) & mask;           
+        end
+
         if( b[width - 1] )begin
-           sign = -1;
-           i_b = ~b + 1;
-        end
-        else begin
-          i_b = b;           
+           sign = -sign;
+           i_b = (~i_b + 1) & mask;           
         end
         
+        // $display( "i_a = %b, i_b = %b\n", i_a, i_b);
+ 
         for( i = 0; i < width - 1; ++i )
           begin
              if( i_b[i] )
                begin
-                  sum = sum + (a << i);
+                  sum = sum + (i_a << i);
                end
           end        
 
-        // deal with sign
+        $display( "raw sum = %b\n", sum );
+        
+        // remove overflow bit
+        sum[2 * width - 1] = 0;
+        
+        // add back sign
         if( sign == -1 )begin
-           out = ~out + 1;
+           sum = ~sum + 1;
         end
-                 
+
+        
+        out = sum;        
+        
+        
      end // always @ ( a, b )
    
 endmodule
--- mul/mul_tb.v
+++ mul/mul_tb.v
@@ -23,16 +23,16 @@ module mul_tb;
    
    initial begin 
       
-	  for(x_cnt = -range; x_cnt < range; x_cnt = x_cnt+1) begin
-		 for(y_cnt = -range; y_cnt < range; y_cnt = y_cnt+1) begin
+	  for(x_cnt = -range + 1; x_cnt < range; x_cnt = x_cnt+1) begin
+		 for(y_cnt = -range + 1; y_cnt < range; y_cnt = y_cnt+1) begin
             x[width - 2: 0] = x_cnt[width - 2: 0];
             y[width - 2: 0] = y_cnt[width - 2: 0];
             x[width - 1] = (x_cnt < 0)? 1: 0;
             y[width - 1] = (y_cnt < 0)? 1: 0;
             
 			#1000
-				     $display("(%2d,%2d) x=%b, y=%b, out=%b", x_cnt, y_cnt, x, y, out);
-			if( out != x * y)
+				     $display("(%2d,%2d) x=%b, y=%b, out=%b, ans=%b", x_cnt, y_cnt, x, y, out, x_cnt * y_cnt);
+			if( out != ((x_cnt * y_cnt) & 4095))
               begin
 			     $display(" FAILED");
                  err += 1;                 
