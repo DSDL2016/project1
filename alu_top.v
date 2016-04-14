14e93e9f216163861a426561aaa2b5536534c9b2
Liu Yen-Ting <windows.linux.mac@gmail.com>
2016-04-14 12:47:45 +0800

    Change module hierarchy

--- alu_top.v
+++ alu_top.v
@@ -1,33 +1,41 @@
 module alu_top #(
-	parameter width = 6
+    parameter width = 6
 )(
-	input						clk,
-	input  [width-1:0] 	a,
-	input  [width-1:0] 	b,
-	input  [1:0] 			func,
-	output [2*width-1:0] out,
-	output 					ovf
+    input                    clk,
+    input      [width-1:0]   a,
+    input      [width-1:0]   b,
+    input      [1:0]         func,
+    output reg [2*width-1:0] out,
+    output                   ovf
 );
-	
-	wire add_out, sub_out, mul_out, div_out;
-	wire add_ovf, sub_ovf, mul_ovf, div_ovf;
-	
-	add_top add(
-		.a			 (a),
-		.b			 (b),
-		.out		 (add_out),
-		.overflow (add_ovf)
-	);
-	
-	sub_top sub(
-		.a			 (a),
-		.b 		 (b),
-		.out		 (sub_out),
-		.overflow (sub_ovf)
-	);
 
-	assign out = (func == 2'b00) ? add_out : 
-					 (func == 2'b01) ? sub_out : 2'bzz;
-	assign overflow = add_ovf | sub_ovf;
+    // wires for module output
+    wire add_out, sub_out, mul_out, div_out;
+    // wires for overflow signals
+    wire add_ovf, sub_ovf, mul_ovf, div_ovf;
+
+    add_top add(
+        .a   (a),
+        .b   (b),
+        .out (add_out),
+        .ovf (add_ovf)
+    );
+
+    sub_top sub(
+        .a   (a),
+        .b   (b),
+        .out (sub_out),
+        .ovf (sub_ovf)
+    );
 	
+    always @(*) begin
+        case(func)
+            2'b00: out = add_out;
+            2'b01: out = sub_out;
+            2'b10: out = mul_out;
+            2'b11: out = div_out;
+        endcase
+    end		  
+    assign overflow = add_ovf | sub_ovf;
+
 endmodule
