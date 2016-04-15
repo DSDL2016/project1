bcb2dba9d355f3c9f621a68a9ca4718bec76d4c5
Liu Yen-Ting <windows.linux.mac@gmail.com>
2016-04-15 20:31:52 +0800

    Tweak test bench for namings and test logic

--- test/add_test.v
+++ test/add_test.v
@@ -1,68 +1,74 @@
 module add_test_bench();
-  parameter WIDTH = 6;
-  // 6-bit sign interger range
-  parameter LOWER = -(1 << (WIDTH - 1));
-  parameter UPPER = (1 << (WIDTH - 1)) - 1;
-  
-  reg cin;
-  reg signed [WIDTH-1:0] a, b;
-  
-  wire w_cin, overflow;
-  wire [WIDTH-1:0] w_a;
-  wire [WIDTH-1:0] w_b;
-  wire signed [WIDTH-1:0] wout;
-  assign w_cin = cin;
-  assign w_a = a;
-  assign w_b = b;
 
-  add_top add(cin, a, b, wout, overflow);
-  
-  integer test_cin, num_a, num_b, sum;
-  initial begin
-    // $display("range from %d %d", LOWER, UPPER);
-	 test_cin = 0;
-    //for (test_cin = 0; test_cin <= 1; test_cin = test_cin + 1) begin
-      for (num_a = LOWER; num_a <= UPPER; num_a = num_a + 1) begin
-        for (num_b = LOWER; num_b <= UPPER; num_b = num_b + 1) begin
-          sum = num_a + num_b;
-          cin = test_cin[0];
-          a = num_a[WIDTH-1:0];
-          b = num_b[WIDTH-1:0];
-          
-          # 10
-          
-          if (num_a != a || num_b != b) begin
-            $display("Input error.");
-            $display("Test Data:%3d+ %3d = %3d", num_a, num_b, sum);
-            $display("Your result:%3d+ %3d = %d", a, b, wout);
-            $display("overflow bit=%b", overflow);
-          end
+    parameter WIDTH = 6;
 
-          if (sum >= LOWER && sum <= UPPER) begin
-            if (overflow == 1) begin  
-              $display("The result isn't overflow, but the overflow bit is set.");
-              $display("Test Data:%3d+ %3d = %3d", num_a, num_b, sum);
-              $display("Your result:%3d+ %3d = %d", a, b, wout);
-              $display("overflow bit=%b", overflow);
-            end else if (sum != wout) begin
-              $display("Summation error");
-              $display("Test Data:%3d+ %3d = %3d", num_a, num_b, sum);
-              $display("Your result:%3d+ %3d = %d", a, b, wout);
-              $display("overflow bit=%b", overflow);
+    // 6-bit sign interger range
+    parameter LOWER = -(1 << (WIDTH - 1));
+    parameter UPPER = (1 << (WIDTH - 1)) - 1;
+
+    reg                    r_cin;
+    reg signed [WIDTH-1:0] r_a, r_b;
+
+    wire                    w_cin, overflow;
+    wire signed [WIDTH-1:0] w_a, w_b;
+    wire signed [WIDTH-1:0] w_out;
+
+    // bond the input to registers
+    assign w_cin = r_cin;
+    assign w_a = r_a;
+    assign w_b = r_b;
+
+    // instantiate the module
+    add_top add(w_cin, w_a, w_b, w_out, overflow);
+
+    // t_* for generated test
+    // w_* for wire connections
+    // r_* for registers
+    integer t_a, t_b, t_out;
+
+    initial begin
+        $display("value can range from %d to %d", LOWER, UPPER);
+
+        // default c_in to 0 since it's for ADD
+        r_cin = 0;
+
+        for (t_a = LOWER; t_a <= UPPER; t_a = t_a+1) begin
+            for (t_b = LOWER; t_b <= UPPER; t_b = t_b+1) begin
+                t_out = t_a + t_b;
+
+                r_a = t_a[WIDTH-1:0];
+                r_b = t_b[WIDTH-1:0];
+
+                // wait for the device to settle
+                # 10
+
+                if (t_out >= LOWER && t_out <= UPPER) begin
+                    // output in range
+                    if (overflow == 1) begin  
+                        $display("\nThe result isn't overflow, but the overflow bit is set.");
+                        $display("Test Data  : %3d+ %3d = %3d", t_a, t_b, t_out);
+                        $display("Your result: %3d+ %3d = %3d", w_a, w_b, w_out);
+                        $display("overflow bit = %b", overflow);
+                    end
+                    else if (w_out != t_out) begin
+                        $display("\nSummation error");
+                        $display("Test Data  : %3d+ %3d = %3d", t_a, t_b, t_out);
+                        $display("Your result: %3d+ %3d = %3d", w_a, w_b, w_out);
+                        $display("overflow bit = %b", overflow);
+                    end
+                end
+                else begin
+                    // output overflow
+                    if (overflow == 0) begin
+                        $display("\nThe result is overflow, but the overflow bit isn't set.");
+                        $display("Test Data  : %3d+ %3d = %3d", t_a, t_b, t_out);
+                        $display("Your result: %3d+ %3d = %3d", w_a, w_b, w_out);
+                        $display("overflow bit = %b", overflow);
+                    end
+                end
             end
-          end else begin
-            if (overflow == 0) begin
-              $display("The result is overflow, but the overflow bit isn't set.");
-              $display("Test Data:%3d+ %3d = %3d", num_a, num_b, sum);
-              $display("Your result:%3d+ %3d = %d", a, b, wout);
-              $display("overflow bit=%b", overflow);
-            end
-          end
-
         end
-      end
-    //end  
-    
-  end
+    end
+
 endmodule
 
