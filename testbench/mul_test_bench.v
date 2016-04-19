51d81d497cc61d7eab466aec9452957083cae399
bookgin <dongsheoil@gmail.com>
2016-04-19 15:15:44 +0800

    finish MUL test, but the test did'nt passed

--- testbench/mul_test_bench.v
+++ testbench/mul_test_bench.v
@@ -7,14 +7,13 @@ module mul_tb;
    localparam range  = 32; // This should be 2^{wdith - 1}
 
    reg  [width-1:0] x, y;
+   reg signed [width*2-1:0] reg_out;
    wire [width*2-1:0] out;
    wire             c_out;
-   reg              sel;
    
-   mul mul(
+   mul_top mul(
 		         .a        (x), 
 		         .b        (y), 
-		         .sel      (sum), 
 		         .out (out)
 	             );
 
@@ -23,21 +22,23 @@ module mul_tb;
    
    initial begin 
       
-	  for(x_cnt = -range + 1; x_cnt < range; x_cnt = x_cnt+1) begin
-		 for(y_cnt = -range + 1; y_cnt < range; y_cnt = y_cnt+1) begin
+	  for(x_cnt = -range; x_cnt < range; x_cnt = x_cnt+1) begin
+		 for(y_cnt = -range; y_cnt < range; y_cnt = y_cnt+1) begin
             x[width - 2: 0] = x_cnt[width - 2: 0];
             y[width - 2: 0] = y_cnt[width - 2: 0];
             x[width - 1] = (x_cnt < 0)? 1: 0;
             y[width - 1] = (y_cnt < 0)? 1: 0;
             
 			#1000
-				     $display("(%2d,%2d) x=%b, y=%b, out=%b, ans=%b", x_cnt, y_cnt, x, y, out, x_cnt * y_cnt);
+			     reg_out = out;
+				   //$display("------out = %b %d", out, reg_out);
 			if( out != ((x_cnt * y_cnt) & 4095))
               begin
-			     $display(" FAILED");
-                 err += 1;                 
+			     //$display(" FAILED");
+			     $display("error:(%2d,%2d=%6d) x=%b, y=%b, out=%b, ans=%b", x_cnt, y_cnt, x_cnt * y_cnt, x, y, out, x_cnt * y_cnt);
+                 err = err + 1;                 
               end
-			$display("\n");
+			//$display("\n");
 		 end
 	  end
 
