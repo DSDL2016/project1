15449a3258b2a8a5fdba941254d7e0888862fc3f
tinray <b03902072@ntu.edu.tw>
2016-04-21 16:53:13 +0800

    Revert "fix mul problem when a == -32 or b == -32"
    This reverts commit 2633eb01c448d0cf695b0e831c9b77cc6b1eb9c6.

--- mul/mul_top.v
+++ mul/mul_top.v
@@ -1,8 +1,8 @@
 module mul_top #(
-	parameter width = 6
+    parameter width = 6
 )(
-	input [width-1:0]      a, b,
-	input                  sel,
+    input      [width-1:0]   a, b,
+    input                    sel,
     output reg [width*2-1:0] out
 );
 
@@ -11,18 +11,16 @@ module mul_top #(
    integer                 sign = 1;
    integer                 i_a;
    integer                 i_b;
-   integer                 mask = 63;
-
+   integer                 mask = 31;
+                
    
    always @( a, b )   
      begin         
         sum = 0;
-        i_a = a[width - 1: 0];           
-        i_b = b[width - 1: 0];           
+        i_a = a[width - 2: 0];           
+        i_b = b[width - 2: 0];           
         sign = 1;        
-        
-        // $display( "i_a = %b, i_b = %b\n", i_a, i_b);
-        
+
         // extract the number part
         if( a[width - 1] )begin
            sign = -sign;
@@ -33,10 +31,8 @@ module mul_top #(
            sign = -sign;
            i_b = (~i_b + 1) & mask;           
         end
-        
-        // $display( "i_a = %b, i_b = %b\n", i_a, i_b);
- 
-        for( i = 0; i < width; ++i )
+
+        for(i = 0; i < width - 1; i = i+1)
           begin
              if( i_b[i] )
                begin
@@ -44,7 +40,7 @@ module mul_top #(
                end
           end        
 
-        $display( "i_a = %b, i_b = %b, raw sum = %b\n", i_a, i_b, sum );
+        $display( "raw sum = %b\n", sum );
         
         // remove overflow bit
         sum[2 * width - 1] = 0;
@@ -54,10 +50,8 @@ module mul_top #(
            sum = ~sum + 1;
         end
 
-        
         out = sum;        
-        
-        
+         
      end // always @ ( a, b )
    
 endmodule
