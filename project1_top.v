c51be8f6d14c1d0c99ab6368ab1b7a9ac6f71f35
Liu Yen-Ting <windows.linux.mac@gmail.com>
2016-05-02 17:44:09 +0800

    Isolate quotient and remainder router

--- project1_top.v
+++ project1_top.v
@@ -14,26 +14,6 @@ module project1_top #(
 	 wire [1:0]         mod_sel = func[1:0];
 	 wire [2*width-1:0] results;
 	 
-	 // using these wires to route 7seg display
-	 // ...starts from HEX0 to HEX7
-	 reg [4*n_segs-1:0] w_bcd;
-	 wire [0:7*n_segs-1] w_segs;
-
-	 always @(*) begin
-	     // show a/b when out_sel is 1
-		  leds = (out_sel) ? {a, b} : results;
-		  
-		  // show separate digits when out_sel is 1 or in divide mode
-        if (out_sel || mod_sel == 2'b11) begin
-				w_bcd = {w_bcd_b1, w_bcd_b10, w_bcd_bsgn, {4{1'b1}}, 
-				         w_bcd_a1, w_bcd_a10, w_bcd_asgn, {4{1'b1}}};
-		  end
-		  else begin
-				w_bcd = {w_bcd_c1, w_bcd_c10, w_bcd_c100, w_bcd_c1000,
-				         w_bcd_csgn, {4{1'b1}}, {4{1'b1}}, {4{1'b1}}};
-		  end
-    end
-	 
 	 //
 	 // alu
 	 //
@@ -50,25 +30,74 @@ module project1_top #(
 	 //
 	 wire [3:0] w_bcd_a1, w_bcd_a10, w_bcd_asgn;
 	 wire [3:0] w_bcd_b1, w_bcd_b10, w_bcd_bsgn;
+	 wire [3:0] w_bcd_q1, w_bcd_q10, w_bcd_qsgn;
+	 wire [3:0] w_bcd_r1, w_bcd_r10, w_bcd_rsgn;
 	 wire [3:0] w_bcd_c1, w_bcd_c10, w_bcd_c100, w_bcd_c1000, w_bcd_csgn;
+	 
 	 bin2bcd conv_a(
 	     .bin     (a),
 		  .bcd     ({w_bcd_a10, w_bcd_a1}),
 		  .bcd_sgn (w_bcd_asgn)
 	 );
+	 
 	 bin2bcd conv_b(
 	     .bin     (b),
 		  .bcd     ({w_bcd_b10, w_bcd_b1}),
 		  .bcd_sgn (w_bcd_bsgn)
 	 );
+	 
 	 bin2bcd #(
 		  .width  (2*width),
 		  .digits (4)
-	 ) conv_c (
+	 ) conv_c(
 	     .bin     (results),
 		  .bcd     ({w_bcd_c1000, w_bcd_c100, w_bcd_c10, w_bcd_c1}),
 		  .bcd_sgn (w_bcd_csgn)
 	 );
+	 
+	 bin2bcd conv_q(
+		  .bin     (results[2*width-1:width]),
+		  .bcd     ({w_bcd_q10, w_bcd_q1}),
+		  .bcd_sgn (w_bcd_qsgn)
+	 );
+	 
+	 bin2bcd conv_r(
+		  .bin     (results[width-1:0]),
+		  .bcd     ({w_bcd_r10, w_bcd_r1}),
+		  .bcd_sgn (w_bcd_rsgn)
+	 );
+	 
+	 // using these wires to route 7seg display
+	 // ...starts from HEX0 to HEX7
+	 reg [4*n_segs-1:0] r_bcd;
+	 wire [0:7*n_segs-1] w_segs;
+	
+	 //
+	 // display selection
+	 //
+	 always @(*) begin
+	     if (out_sel) begin
+		      leds = {a, b};
+				
+				r_bcd = {w_bcd_b1, w_bcd_b10, w_bcd_bsgn, {4{1'b1}}, 
+				         w_bcd_a1, w_bcd_a10, w_bcd_asgn, {4{1'b1}}};
+		  end
+		  else begin
+		      leds = results;
+				
+		      if (mod_sel == 2'b11) begin
+				    // divide mode
+					 r_bcd = {w_bcd_r1, w_bcd_r10, w_bcd_rsgn, {4'b{1'b1}},
+								 w_bcd_q1, w_bcd_q10, w_bcd_qsgn, {4{1'b1}}};
+				end
+				else begin
+					 r_bcd = {w_bcd_c1, w_bcd_c10, w_bcd_c100, w_bcd_c1000,
+								 w_bcd_csgn, {4{1'b1}}, {4{1'b1}}, {4{1'b1}}};
+				end
+		  end
+    end
+	 
+	 // re-route quotion and remainder signal
 	 
 	 //
 	 // bcd2seg
@@ -77,12 +106,13 @@ module project1_top #(
 	 generate
 	     for (i = 0; i < n_segs; i = i+1) begin : BCD2SEG_CONCAT
 		      bcd2seg conv_seg(
-				    .bcd (w_bcd[(n_segs-i)*4-1 -: 4]),
+				    .bcd (r_bcd[(n_segs-i)*4-1 -: 4]),
 					 .seg (w_segs[i*7 +: 7])
 				);
 		  end
 	 endgenerate
 	 
+	 // invert the output for HEX displays
 	 assign segs = ~w_segs;
 	 	 
 endmodule
